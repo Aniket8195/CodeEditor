491ad71ca4f427c24099266148cd275032da49ea
Aniket8195 <9518aniket@gmail.com>
2024-02-07 21:48:52 +0530

    Test Cases Running Properly

--- lib/Screens/ProblemScreen/bloc/Compiler/compiler_bloc.dart
+++ lib/Screens/ProblemScreen/bloc/Compiler/compiler_bloc.dart
@@ -11,19 +11,37 @@ part 'compiler_state.dart';
 
 class CompilerBloc extends Bloc<CompilerEvent, CompilerState> {
   CompilerBloc() : super(CompilerInitial()) {
-    on<CompilerSubmit>((event, emit) {
+    on<CompilerSubmit>((event, emit) async {
        emit(CompilerSubmitState());
         try {
-          if(event.problem.testCases.isNotEmpty){
-            List<bool>res=[];
-            for(int i=0;i<event.problem.testCases.length;i++){
-              compileCode(event.problem.testCases[i].input,event.problem.testCases[i].output,event.language,event.code).then((value) {
-                res.add(value);
-              });
+          // if(event.problem.testCases.isNotEmpty){
+          //   List<bool>res=[];
+          //   for(int i=0;i<event.problem.testCases.length;i++){
+          //     compileCode(event.problem.testCases[i].input,event.problem.testCases[i].output,event.language,event.code).then((value) {
+          //       res.add(value);
+          //     });
+          //   }
+          //   emit(CompilerDone(testCases:event.problem.testCases,testCaseResults:res));
+          // }else{
+          //  emit(CompilerError(message:"List is empty"));
+          // }
+          if (event.problem.testCases.isNotEmpty) {
+            List<bool> testResults = [];
+            for (final testCase in event.problem.testCases) {
+              final result = await compileCode(
+                testCase.input,
+                testCase.output,
+                event.language,
+                event.code,
+              );
+              testResults.add(result);
             }
-            emit(CompilerDone(testCases:event.problem.testCases,testCaseResults:res));
-          }else{
-           emit(CompilerError(message:"List is empty"));
+            emit(CompilerDone(
+              testCases: event.problem.testCases,
+              testCaseResults: testResults,
+            ));
+          }else {
+            emit(CompilerError(message: "List is empty"));
           }
         }catch(error){
           emit(CompilerError(message:error.toString()));
@@ -32,6 +50,7 @@ class CompilerBloc extends Bloc<CompilerEvent, CompilerState> {
   }
   Future<bool>compileCode(String input,String output,String language,String code)async{
     bool res=false;
+    print("Compiling code for input: $input");
     Map<String, dynamic> requestBody = {
       "code": code,
       "input": input,
@@ -44,10 +63,15 @@ class CompilerBloc extends Bloc<CompilerEvent, CompilerState> {
         headers: {'Content-Type': 'application/json'},
       );
       if (response.statusCode == 200) {
+        print(response.body);
         Map<String, dynamic> responseBody = jsonDecode(response.body);
-        if(responseBody['message']==output){
+        String extractedOutput = responseBody['message'].split(" '")[1].split("'\r\n")[0];
+        if(extractedOutput==output){
           res=true;
+          print("Test case passed for input: $input");
           print(res);
+        }else{
+          print("Test case failed for input: $input");
         }
       } else {
         throw Exception('Failed to compile code: ${response.statusCode}');
